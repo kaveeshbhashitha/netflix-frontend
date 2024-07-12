764f1545bc2198b64dff87dc12500644b96098d4
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2024-07-12 11:22:41 +0530

    add video function is working

--- src/pages/user/AddVideo.jsx
+++ src/pages/user/AddVideo.jsx
@@ -4,13 +4,35 @@ import logo from '../../images/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faCoins, faFeed, faFlag, faGear, faHouse, faInfoCircle, faList, faPlay, faUpDown } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function AddVideo() {
     const [isNavOpen, setIsNavOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({ youtubeId: '', videoTitle: '', category: '', videoType: '' });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:8080/video/add', formData);
+            if (response.status === 200) {
+                setMessage({text: 'Video added successfully', class: 'alert alert-success'});
+            }else{
+                setMessage({text: 'Video insertion not success', class: 'alert alert-danger'});
+            }
+        } catch (error) {
+            setMessage({ text: 'Error inserting videos', class: 'alert alert-danger' });
+        }
+    };
 
     const toggleNav = () => {
         setIsNavOpen(!isNavOpen);
     };
+
   return (
     <div>
         <div id="mySidenav" style={{ width: isNavOpen ? '0' : '250px' }} className="sidenav">
@@ -45,21 +67,21 @@ export default function AddVideo() {
                     </form>
                 </div>
             </div>
-
+            <div>{ message && <div class={ message.class }>{message.text}</div>}</div>
             <div className='p-4 border rounded'>
             <h5 className="mt-4 mb-4 text-danger">Add New Video</h5>
                 <form action="#" method="post">
                     <div className="form-group mb-3">
                         <label for="name" className='text-dark mb-2'>YouTube Video ID</label>
-                        <input type="text" id="name" name="name" placeholder='Enter youtube ID' className="form-control"/>
+                        <input type="text" name="youtubeId" placeholder='Enter youtube ID' className="form-control" value={formData.youtubeId} onChange={handleChange}/>
                     </div>
                     <div className="form-group mb-3">
                         <label for="title" className='text-dark mb-2'>Video Title</label>
-                        <input type="text" id="title" name="title" placeholder='Enter video title' className="form-control"/>
+                        <input type="text" name="videoTitle" placeholder='Enter video title' className="form-control" value={formData.videoTitle} onChange={handleChange}/>
                     </div>
                     <div className="form-group mb-3">
                         <label for="category" className='text-dark mb-2'>Video Category</label>
-                        <select type="text" id="category" name="category" className="form-control">
+                        <select type="text" name="category" className="form-control" value={formData.category} onChange={handleChange}>
                             <option value="">-- Select movie category --</option>
                             <option value="horror">Horror</option>
                             <option value="action">Action</option>
@@ -72,7 +94,7 @@ export default function AddVideo() {
                     </div>
                     <div className="form-group mb-3">
                         <label for="category" className='text-dark mb-2'>Video Type</label>
-                        <select type="text" id="category" name="videoType" className="form-control">
+                        <select type="text" name="videoType" className="form-control" value={formData.videoType} onChange={handleChange}>
                             <option value="">-- Select movie type --</option>
                             <option value="movie">Movie</option>
                             <option value="documentry">Documentry</option>
@@ -81,7 +103,7 @@ export default function AddVideo() {
                         </select>
                     </div>
                     <div className="d-flex justify-content-end mt-3">
-                        <button type="submit" className="btn btn-outline-danger mx-1">Save Video</button>
+                        <button className="btn btn-outline-danger mx-1" onClick={handleSubmit}>Save Video</button>
                         <a href="/video" className="btn btn-outline-dark">Back</a>
                     </div>
                 </form>
